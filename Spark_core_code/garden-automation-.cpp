cf554d1d0e093145e0d3827919eede61e4ba8a9e
jcomega <jcomega03@gmail.com>
2014-07-27 11:49:22 +0200

    update

--- Spark_core_code/garden-automation-.cpp
+++ Spark_core_code/garden-automation-.cpp
@@ -1,4 +1,3 @@
-/* ======================= Includes ================================= */
 
 #include "DHT22.h"
 
@@ -15,6 +14,7 @@ int STARTPIN = D7;
 int BUTTONPIN = D0;
 int RELAY1 = D5;
 int RELAY2 = D6;
+int VCC_DHT22 = D3;
 int LIGHTPIN = A0;  //LIGHT PIN, photoresistance    
 int TEMPPIN = A1;  //MCP9700A temperature sensor
 int REDLED = A6;
@@ -33,105 +33,113 @@ int lightValue = 1000;
 int Humid_Sol1 = 100;
 int Humid_Sol2 = 200;
 int Water_Level = 300;
+float Battery_Voltage = 0;
 int redLEDetat = OFF;
 int LED_flash_value = 00;
 float temperature_val = 47.8;
+float battery_val = 12.0;
 float h;      // humidity
 float t;      // temperature
 int f = 0;    // failed?
 
-char temperatureValue[35];
+char temperatureValue[43];
 char AM2302_TempValue[30];
 char AM2302_HumidValue[30];
+char battery_VoltValue[35];
+char Water_LevelValue[30];
 
-uint32_t UPDATE_INTERVAL = 10; // in milliseconds
-uint32_t UPDATE_INTERVAL_TEMP = 500; // in milliseconds
-uint32_t FLASH_LED_INTERVAL = 150; // in milliseconds
-uint32_t FIFTEEN_MINUTES = 60*15*1000; // in milliseconds
-uint32_t FIVE_MINUTES = 60*5*1000; // in milliseconds
-uint32_t ONE_MINUTES = 60*1*1000; // in milliseconds
+#define  UPDATE_INTERVAL  100 // in milliseconds
+#define  UPDATE_INTERVAL_TEMP  500 // in milliseconds
+#define  FIFTEEN_MINUTES 60*15*1000 // in milliseconds
+#define  TEN_MINUTES 60*10*1000 // in milliseconds
+#define  FIVE_MINUTES 60*5*1000 // in milliseconds
+#define  TREE_MINUTES 60*3*1000 // in milliseconds
+#define  ONE_MINUTES 60*1*1000 // in milliseconds
+#define  UPDATE_WIFI_ON 60*1000 // in milliseconds
 
 uint32_t lastTime = 0; // in milliseconds
 uint32_t lastTimeTemp = 0; // in milliseconds
 uint32_t lastTimeLED = 0; // in milliseconds
 uint32_t lastTimemoisture = 0; // in milliseconds
+uint32_t lastTimeLong = 0; // in milliseconds
+uint32_t lastTimeWifi = 0; // in milliseconds
 
 uint32_t startTimepump1 = 0; // in milliseconds
 uint32_t startTimepump2 = 0; // in milliseconds
 
 void setup() {
-    pinMode(STARTPIN,OUTPUT);
+
     pinMode(BUTTONPIN,INPUT);
     pinMode(RELAY1,OUTPUT);
     pinMode(RELAY2,OUTPUT);
 	pinMode(LIGHTPIN, INPUT);
 	pinMode(TEMPPIN, INPUT);
+    pinMode(VCC_DHT22,OUTPUT);
+    
     pinMode(REDLED,OUTPUT);
     
     pinMode(MOISTURE_VCC,OUTPUT);
 	pinMode(MOISTURE1_ANA, INPUT);
 	pinMode(MOISTURE2_ANA, INPUT);
 	pinMode(WATER_ANA, INPUT);
+	pinMode(TEST12V_ANA, INPUT);
 
-    digitalWrite(STARTPIN,LOW);
-    //digitalWrite(TRUNKPIN,LOW);
-    //digitalWrite(LOCKPIN,LOW);
-    //digitalWrite(UNLOCKPIN,LOW);
+    digitalWrite(VCC_DHT22,HIGH);
     digitalWrite(REDLED,LOW);
 
     Spark.function("cmdpump1", cmdpump1Function);
-    Spark.function("readbutton", buttonFunction);
+    //Spark.function("readbutton", buttonFunction);
     Spark.function("cmdpump2", cmdpump2Function);
-    Spark.function("unlock", unlockFunction);
+    //Spark.function("unlock", unlockFunction);
     
     Spark.variable("pump1state", &cmdpump1State, INT);
     Spark.variable("buttonstate", &buttonState, INT);
     Spark.variable("pump2state", &cmdpump2State, INT);
     Spark.variable("lightvalue", &lightValue, INT);
     
-    // water temperature MCP9700A
-	Spark.variable("tempvalue", &temperatureValue, STRING);
+    // water temperature MCP9700A + water level
+	Spark.variable("tempvaluew", &temperatureValue, STRING);
 	
 	//resistor sensor
     Spark.variable("humidsol1", &Humid_Sol1, INT);
     Spark.variable("humidsol2", &Humid_Sol2, INT);
+    
+    // 12V battery voltage
+    Spark.variable("batteryvol", &battery_VoltValue, STRING);
 	
 	// am3202 SENSOR :
 	Spark.variable("temperature", &AM2302_TempValue, STRING);
 	Spark.variable("humidity", &AM2302_HumidValue, STRING);
 	dht.begin();
+    lastTimeWifi = millis();
 }
 
 void loop() {
     
-    dht.poll();
+    if(millis() - lastTimeWifi > TEN_MINUTES) {
+    //digitalWrite(REDLED,HIGH);   
+    Spark.sleep(300); //Wifi sleep for 5mn
+    //digitalWrite(REDLED,LOW);
     
+    while(WiFi.status() == 0)
+        {
+             //code stuff
+             delay(10000);
+        }
+      
+    lastTimeWifi = millis();
+    }
+    
+    dht.poll();
     
     // Update every 10ms (default)
     if(millis() - lastTime > UPDATE_INTERVAL) {
         // Set a new last time
         lastTime = millis();
-		
-	
-		//read button and change led D7
-		if(digitalRead(BUTTONPIN) == ON) {
-			//digitalWrite(STARTPIN, LOW); // Single press the start button to stop
-			buttonState = ON; // change variable
-		}
-		else {
-			//digitalWrite(STARTPIN, HIGH); // 
-			buttonState = OFF; // change variable
-		}
-		
-        lightValue =  analogRead(LIGHTPIN);
-        
-		//lightValue =  1500;
-		LED_flash_value = lightValue/10;
-		
-		
+
 		//disable pump1 if this pump is activated since 5 minutes
         if(cmdpump1State == ON) {
-            if(millis() - startTimepump1 > FIVE_MINUTES) {
+            if(millis() - startTimepump1 > TREE_MINUTES) {
                 cmdpump1State = OFF;
                 digitalWrite(RELAY1, LOW);
             }
@@ -139,7 +147,7 @@ void loop() {
         
         //disable pump2 if this pump is activated since 5 minutes
         if(cmdpump2State == ON) {
-            if(millis() - startTimepump2 > FIVE_MINUTES) {
+            if(millis() - startTimepump2 > TREE_MINUTES) {
                 cmdpump2State = OFF;
                 digitalWrite(RELAY2, LOW);
             }
@@ -163,9 +171,26 @@ void loop() {
         delay(100);
         //turn off vcc pin moisture
         digitalWrite(MOISTURE_VCC, LOW);
-        //digitalWrite(MOISTURE2_VCC, LOW);
         
+        //water temperature
+		temperature_val = analogRead(TEMPPIN) * 0.800 ;//(Vana_temp*3300)/4086  // en mV
+		temperature_val = ((temperature_val-500)/10); // Calcul température
+		if(Water_Level>1000)
+		sprintf(temperatureValue, "Reserve eau: %.1f°C || Niveau ok", temperature_val);
+		else
+		sprintf(temperatureValue, "Reserve eau: %.1f°C || Plus d'eau !", temperature_val);
 
+		//sprintf(temperatureValue, "Temperature: %.1f°C (brute=%d)",temperature_val, analogRead(TEMPPIN));
+		
+		//light mesure
+        lightValue =  analogRead(LIGHTPIN);
+    
+        // read, calcul and dispplay battery voltage
+        Battery_Voltage = analogRead(TEST12V_ANA) * 0.005371; //((Vana*3300)/4096)*6.666   // 6.66 ration diviseur de tension avec 68K et 12K
+		//temperature_val = analogRead(TEMPPIN) * 0.800 ;//(Vana_temp*3300)/4086  // en mV
+		//temperature_val = ((temperature_val-500)/10); // Calcul température
+		sprintf(battery_VoltValue, "Tension batterie: %.1fV", Battery_Voltage);
+	
     }
     
     
@@ -174,16 +199,12 @@ void loop() {
         // Set a new last time
         lastTimeTemp = millis();
 		// temperature measure
-		temperature_val = analogRead(TEMPPIN) * 0.800 ;//(Vana_temp*3300)/4086  // en mV
-		temperature_val = ((temperature_val-500)/10); // Calcul température
-		sprintf(temperatureValue, "Température de l'eau: %.1f°C", temperature_val);
-		//sprintf(temperatureValue, "Temperature: %.1f°C (brute=%d)",temperature_val, analogRead(TEMPPIN));
-		
+
 		f = 0;
         h = dht.readHumidity();
         t = dht.readTemperature();
     
-        if (t==NAN || h==NAN) {
+        if (t==NAN || h==NAN || h==0 || t==0){
             f = 1; // not a number, fail.
         }
         else {
@@ -192,26 +213,20 @@ void loop() {
 		sprintf(AM2302_HumidValue, "Humidité: %.1f %%", h);
         }
     }
-	
-	// clignioteur de led en fonction de la lumière
-    if(millis() - lastTimeLED > LED_flash_value) {
+    
+    if(millis() - lastTimeLong > FIFTEEN_MINUTES) {
         // Set a new last time
-        lastTimeLED = millis();
-		if(redLEDState == ON) {
-			if (redLEDetat == ON ) 
-			{
-				digitalWrite(REDLED,LOW);
-				redLEDetat = OFF;
-			}	
-			else 
-			{
-				digitalWrite(REDLED,HIGH);
-				redLEDetat = ON;
-			}	
-        }
-		else digitalWrite(REDLED,LOW);
+        lastTimeLong = millis();
+
+        //turn off vcc DHT11 pin
+        digitalWrite(VCC_DHT22, LOW);
+        delay(500);
+        digitalWrite(VCC_DHT22, HIGH);
+        
+	    dht.begin();
     }
 	
+
 }
 
 int cmdpump1Function(String args) {
@@ -250,35 +265,3 @@ int cmdpump2Function(String args) {
     }
 }
 
-int buttonFunction(String args) {
-    if(digitalRead(BUTTONPIN) == ON) {
-        buttonState = ON; // change variable
-		//digitalWrite(STARTPIN, LOW); // Single press the start button to stop
-    }
-    else {
-        buttonState = OFF; // change variable
-		//digitalWrite(STARTPIN, HIGH); // 
-    }
-    return 200; // This is checked in the web app controller for validation
-}
-
-int lockFunction(String args) {
-    if(redLEDState == OFF) {
-        redLEDState = ON; // locks locked
-        //digitalWrite(LOCKPIN, HIGH); // Single press the lock button
-        //delay(500);
-        //digitalWrite(LOCKPIN, LOW);
-    }
-	else redLEDState = OFF;
-    return 200; // This is checked in the web app controller for validation
-}
-
-int unlockFunction(String args) {
-    if(redLEDState == ON) {
-        redLEDState = OFF; // locks unlocked
-        //digitalWrite(UNLOCKPIN, HIGH); // Single press the unlock button
-        //delay(500);
-        //digitalWrite(UNLOCKPIN, LOW);
-    }
-    return 200; // This is checked in the web app controller for validation
-}
